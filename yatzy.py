64103c533b9181b135666b189c2fee3aaba821b0
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-01-14 00:58:31 +0100

    refactor(logic): Elimina los and de los metodos de escalera y cambialos por un enumerate

--- yatzy.py
+++ yatzy.py
@@ -6,8 +6,8 @@ class Yatzy:
     @staticmethod
     def chance(*dices):
         total = 0
-        for dice in dices:
-            total += dice
+        for pip in dices:
+            total += pip
         return total
 
     @staticmethod
@@ -41,8 +41,8 @@ class Yatzy:
     @staticmethod
     def pair(*dices):
         pairs = [0]*6
-        for dice in dices:
-            pairs[dice-1] += 1
+        for pip in dices:
+            pairs[pip-1] += 1
         for num in range(6):
             if (pairs[6-num-1] >= 2):
                 return (6-num)*2
@@ -51,8 +51,8 @@ class Yatzy:
     @staticmethod
     def two_pairs(*dices):
         pairs = [0]*6
-        for dice in dices:
-            pairs[dice-1] += 1
+        for pip in dices:
+            pairs[pip-1] += 1
         n = 0
         score = 0
         for num in range(6):
@@ -66,47 +66,41 @@ class Yatzy:
     
     @staticmethod
     def three_of_a_kind(*dices):
-        for dice in range(6):
-            if dices.count(dice) >= 3:
-                return dice * 3
+        for pip in range(6):
+            if dices.count(pip) >= 3:
+                return pip * 3
         return 0
 
     @staticmethod
     def four_of_a_kind(*dices):
-        for dice in range(6):
-            if dices.count(dice) >= 4:
-                return dice * 4
+        for pip in range(6):
+            if dices.count(pip) >= 4:
+                return pip * 4
         return 0
 
     @staticmethod
-    def small_straight(dice1,  dice2,  dice3,  dice4,  dice5):
-        if (dice1 == 1 and
-            dice2 == 2 and
-            dice3 == 3 and
-            dice4 == 4 and
-            dice5 == 5):
-            return 15
-        return 0
+    def small_straight(*dices):
+        for die, pip in enumerate(sorted(dices), 1):
+            if die != pip:
+                return 0
+        return 15
+        # return [0 for die, pip in enumerate(sorted(dices), 1) if die != pip else 15]
 
     @staticmethod
-    def large_straight( dice1,  dice2,  dice3,  dice4,  dice5):
-        if (dice1 == 2 and
-            dice2 == 3 and
-            dice3 == 4 and
-            dice4 == 5 and
-            dice5 == 6):
-            return 20
-        return 0
+    def large_straight(*dices):
+        for die, pip in enumerate(sorted(dices), 2):
+            if die != pip:
+                return 0
+        return 20
     
-
     @staticmethod
     def full_house(*dices):
         pair = False
         three = False
         score = 0
         full = [0]*6
-        for dice in dices:
-            full[dice-1] += 1
+        for pip in dices:
+            full[pip-1] += 1
         for num in range(6):
             if (full[num] == 2): 
                 pair = True
